cbd54605881202c147b1b9cd5625354e0a853301
rash0x6964 <rash0x6964@gmail.com>
2023-10-03 06:40:51 -0700

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for n1 in range(0, 9):
+    for n2 in range(1, 10):
+        if n1 == 8 and n2 == 9:
+            print("{}{}".format(n1, n2))
+        else:
+            print("{}{}".format(n1, n2), end=", ")
