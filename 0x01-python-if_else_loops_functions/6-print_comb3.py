3a1294b92de1dfe8d31022e220c4ad1a19ccb62e
rash0x6964 <rash0x6964@gmail.com>
2023-10-03 06:43:19 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for n1 in range(0, 9):
     for n2 in range(1, 10):
-        if n1 == 8 and n2 == 9:
-            print("{}{}".format(n1, n2))
-        else:
+        if n2 > n1 and n1 != 8:
             print("{}{}".format(n1, n2), end=", ")
+        elif n1 == 8 and n2 == 9:
+            print("{}{}".format(n1, n2))
