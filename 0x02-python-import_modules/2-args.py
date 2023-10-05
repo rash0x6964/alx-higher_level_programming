eaa0c9f09d37de5b78b741d7be3e7afa42876d99
rash0x6964 <rash0x6964@gmail.com>
2023-10-05 06:27:04 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+i = 1
+sys.argv.pop(0)
+l = len(sys.argv)
+
+if __name__ == '__main__':
+    if l == 0:
+        print('0 arguments.')
+    if len(sys.argv) > 0:
+        if l == 1:
+            print('1 argument:')
+        else:
+            print('{} arguments:'.format(l))
+        for arg in sys.argv:
+            print('{}: {}'.format(i, arg))
+            i+=1
