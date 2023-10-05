608ab98b96b913d6f5b43718c14d081a54b341ca
rash0x6964 <rash0x6964@gmail.com>
2023-10-05 06:36:32 -0700

    fix pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,15 @@
 import sys
 i = 1
 sys.argv.pop(0)
-l = len(sys.argv)
+length = len(sys.argv)
 if __name__ == '__main__':
-    if l == 0:
+    if length == 0:
         print('0 arguments.')
     if len(sys.argv) > 0:
-        if l == 1:
+        if length == 1:
             print('1 argument:')
         else:
-            print('{} arguments:'.format(l))
+            print('{} arguments:'.format(length))
         for arg in sys.argv:
             print('{}: {}'.format(i, arg))
-            i+=1
+            i += 1
