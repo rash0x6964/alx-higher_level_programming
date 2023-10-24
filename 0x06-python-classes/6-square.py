0544d62bff9e3b6d5c24491e212c88f139db3505
rash0x6964 <rash0x6964@gmail.com>
2023-10-24 14:35:34 -0700

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,32 +6,32 @@ class Square():
 	''' the class square '''
 
 	def __init__(self, size=0, position=(0, 0)):
-	    '''Initialize a new square.'''
-	    self.size = size
-	    self.position = position
+		'''Initialize a new square.'''
+		self.size = size
+		self.position = position
 
 	@property
 	def size(self):
-	    return self.__size
+		return self.__size
 
 	@size.setter
 	def size(self, value):
-	    if type(value) != int:
-	        raise TypeError("size must be an integer")
-	    elif value < 0:
-	        raise ValueError("size must be >= 0")
-	    else:
-	        self.__size = value
+		if type(value) != int:
+			raise TypeError("size must be an integer")
+		elif value < 0:
+			raise ValueError("size must be >= 0")
+		else:
+			self.__size = value
 
 	@property
 	def position(self):
-	    return self.__position
+		return self.__position
 
 	@position.setter
 	def position(self, value):
-	    if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(num, int) and num >= 0 for num in value):
-	        raise TypeError("position must be a tuple of 2 positive integers")
-	    self.__position = value
+		if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(num, int) and num >= 0 for num in value):
+			raise TypeError("position must be a tuple of 2 positive integers")
+		self.__position = value
 
 	def area(self):
 		return (self.__size * self.__size)
