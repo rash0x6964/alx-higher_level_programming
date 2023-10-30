f97592010c00e5dfdfd6a6f65ffdd7e8f36acce8
rash0x6964 <rash0x6964@gmail.com>
2023-10-30 19:56:46 +0100

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """property setter to set it"""
+        """property to set the value"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -30,7 +30,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """property to set the value"""
+        """property setter to set it"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
@@ -48,3 +48,14 @@ class Rectangle:
             return 0
         else:
             return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """print the rectangle with the character #"""
+        if self.__height == 0 or self.__width == 0:
+            return ("")
+        char = []
+        for i in range(self.__height):
+            char.append('#' * self.__width)
+            if i != self.__height - 1:
+                char.append("\n")
+        return ("".join(char))
