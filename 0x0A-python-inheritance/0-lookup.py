f038011985f6b444790e6f5cf9c52d29416e578c
rash0x6964 <rash0x6964@gmail.com>
2024-02-05 21:40:19 +0100

    ...

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Define a lookup function"""
+
 
 def lookup(obj):
-    """ Get the list of available attributes and methods of an object."""
+    """Get the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        A list containing the names of attributes and methods.
+
+    """
     return dir(obj)
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
+"""Define a class inherits from list class"""
+
+
 class MyList(list):
+    """MyList class that inherits from list"""
+
     def print_sorted(self):
+        """Class methods that prints the list, but sorted.
+
+        Returns:
+            Nothing
+
+        """
         cpy = self.copy()
         cpy.sort()
         print(cpy)
