70cbcbbbaf0cbe0840fdfc2800b3fde099657885
rash0x6964 <rash0x6964@gmail.com>
2024-02-11 16:15:17 +0100

    task 11 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@ class Square(Rectangle):
     """A class that defines a square"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
@@ -15,6 +16,32 @@ class Square(Rectangle):
         """
         return (
             f"[Square] ({self.id}) "
-            f"<{self.__x}>/<{self.__y}> - "
-            f"<{self.__width}>"
+            f"{self.x}/{self.y} - "
+            f"{self.width}"
         )
+
+    @property
+    def size(self):
+        """property to retrieve the value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property to set the value"""
+        self.width = value
+        self.height = value
+        self.__size = value
+
+    def update(self, *args, **kwargs):
+        """Update the attributes of the square.
+
+        Args:
+            *args: A sequence of arguments
+        """
+        if args and len(args) != 0:
+            attr = ["id", "size", "x", "y"]
+            for index, value in enumerate(args):
+                setattr(self, attr[index], value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
