b09be4937b638740172bfb73a97fa3ee3ceb1a29
rash0x6964 <rash0x6964@gmail.com>
2024-04-12 10:36:01 +0000

    make the script accept params

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="localhost",
-        user="root",
-        passwd="root",
-        db="hbtn_0e_0_usa"
+        host= "localhost",
+        user= sys.argv[1],
+        passwd= sys.argv[2],
+        db= sys.argv[3]
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
