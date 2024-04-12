42d6161621d4e41102e7bdf3fd267f4b80186d61
rash0x6964 <rash0x6964@gmail.com>
2024-04-12 14:15:11 +0000

    First state model

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script that takes in the name of a state as an argument
-	and lists all cities of that state,
+    and lists all cities of that state,
     using the database hbtn_0e_4_usa
 """
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         JOIN states ON cities.state_id = states.id \
         WHERE states.name LIKE BINARY %s \
         ORDER BY cities.id ASC",
-		(sys.argv[4],)
+        (sys.argv[4],)
     )
     rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
