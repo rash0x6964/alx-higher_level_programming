4d868541bfa8ba4c59dbfef7d137f24cfd622d3b
rash0x6964 <rash0x6964@gmail.com>
2024-05-21 12:47:52 +0100

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) return;
+  const resp = {};
+  const todos = JSON.parse(body);
+  const completedTasks = todos.reduce((acc, todo) => {
+    if (todo.completed) { acc[todo.userId] = (acc[todo.userId] || 0) + 1; }
+    return acc;
+  }, {});
+
+  console.log(completedTasks);
+});
